5e6357105b3b65fc9518f746c8194245af53de70
Oleg Pozdniakov <oapfozzy.com@MacBook-Air.local>
2023-11-05 17:57:01 +0200

    2

--- app.js
+++ app.js
@@ -15,13 +15,13 @@ const blocks = document.querySelectorAll('.blk')
 for (var item of blocks){
     item.addEventListener('mouseover', function(event){
         event.target.style.border = '3px solid red'
+        event.target.style["boxShadow"] = " 1px 1px 96px 51px rgba(255,255,255,0.75)"
     })
     item.addEventListener('mouseout', function(event){
         event.target.style.border = '3px solid rgba(160, 160, 0, 0.088)'
+        event.target.style["boxShadow"] = "none"
     })
-    item.addEventListener('touchstart', function(event){
-        event.target.style.border = '3px solid rgba(160, 160, 0, 0.088)'
-    })
+
 }
 
 
