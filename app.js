391a983a183b6894047d4bb517136eec41e0a7a7
Oleh Pozdniakov <oleh@test.io>
2023-11-05 20:38:38 +0200

    v5

--- app.js
+++ app.js
@@ -1,39 +1,48 @@
-const block = document.getElementById('blk')
-const container = document.getElementById('container')
-const timerContainer = document.getElementById('timer-container')
+const block = document.getElementsByClassName("blk")[0];
+const container = document.getElementById("container");
+const timerContainer = document.getElementById("timer-container");
 
+tileCount = Math.floor(
+    (window.innerWidth / block.offsetWidth) *
+        (window.innerHeight / block.offsetHeight)
+);
 
-draw(1000)
+function calculateTiles() {
+    const tilesPerRow = window.innerWidth / block.offsetWidth;
+    const aspectRatio = window.innerWidth / window.innerHeight;
+    const tilesPerCol = tilesPerRow / aspectRatio;
+    return Math.floor(tilesPerRow * tilesPerCol + 10);
+}
 
-function draw (count){
-    for (var i = 0; i <= count; i++)
-    {
-        container.insertAdjacentHTML("beforeend", '<div class="blk"></div>')
-        console.log("element")
+draw(calculateTiles());
+
+function draw(count) {
+    for (var i = 0; i <= count; i++) {
+        container.insertAdjacentHTML("beforeend", '<div class="blk"></div>');
     }
 }
-const blocks = document.querySelectorAll('.blk')
+const blocks = document.querySelectorAll(".blk");
 
-for (var item of blocks){
-    item.addEventListener('mouseover', function(event){
-        event.target.style.border = '3px solid white'
-        event.target.style["boxShadow"] = " 1px 1px 96px 51px rgba(255,255,255,0.75)"
-    })
-    item.addEventListener('mouseout', function(event){
-        event.target.style.border = '3px solid rgb(26, 26, 26)'
-        event.target.style["boxShadow"] = "none"
-    })
+for (var item of blocks) {
+    item.addEventListener("mouseover", function (event) {
+        event.target.style.border = "3px solid white";
+        event.target.style["boxShadow"] =
+            " 1px 1px 96px 51px rgba(255,255,255,0.75)";
+    });
+    item.addEventListener("mouseout", function (event) {
+        event.target.style.border = "3px solid rgb(26, 26, 26)";
+        event.target.style["boxShadow"] = "none";
+    });
 }
 
-renderTime()
-function renderTime()
-{
-    now = new Date()
-    const divider = now.getSeconds()%2 === 0 ? ' ' : ':'
-    timerContainer.innerHTML = now.getHours() + divider + (now.getMinutes() < 10 ? '0' : '') + now.getMinutes()
+renderTime();
+function renderTime() {
+    now = new Date();
+    const divider = now.getSeconds() % 2 === 0 ? " " : ":";
+    timerContainer.innerHTML =
+        now.getHours() +
+        divider +
+        (now.getMinutes() < 10 ? "0" : "") +
+        now.getMinutes();
 }
-const mainTimer = setInterval(renderTime, 1000)
-
-
-
-
+const mainTimer = setInterval(renderTime, 1000);
